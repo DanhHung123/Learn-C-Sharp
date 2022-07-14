20545f944eaf8d28261817f4438218262e3b77ec
DanhHung123 <91303551+DanhHung123@users.noreply.github.com>
2022-07-14 10:05:28 +0700

    bo sung phan in ra

--- Exam/Exam/Program.cs
+++ Exam/Exam/Program.cs
@@ -67,7 +67,7 @@ class Program
     {
         // Array Product (3 book, 2 phone)
         Product[] product = new Product[5];
-        product[0] = new Book(1, "Dac nhan tam", 300);
+        product[0] = new Book(1, "Dac nhan tam", 300); 
         product[1] = new Book(2, "Cha giau cha ngheo",450);
         product[2] = new Book(3, "Nha gia kim", 500);
         product[3] = new Phone(4, "Iphone 10", 2220);
@@ -77,9 +77,11 @@ class Program
         double totalTax = 0;
         foreach(var p in product)
         {
+            Console.WriteLine($"{p.Name}: {p.computeTax()}$");
             totalTax += p.computeTax();
         }
         // Print total tax
-        Console.WriteLine(totalTax);
+        Console.WriteLine();
+        Console.WriteLine($"=> Total tax: {totalTax}$" );
     }
 }
